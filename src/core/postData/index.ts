44a45947586f9d7161ab065bc1181f438a59e972
Cafe <18375006@qq.com>
2019-06-02 10:10:09 +0800

    feat: upgrade `ts-node` and `typescript`.

--- src/core/postData/index.ts
+++ src/core/postData/index.ts
@@ -2,7 +2,7 @@ import { Context } from '@core/koa'
 
 // only post fields(not upload file stream)
 
-const getPostData = (ctx: Context) => {
+const getPostData = (ctx: Context): object => {
 
   return new Promise((resolve, reject) => {
     try{
